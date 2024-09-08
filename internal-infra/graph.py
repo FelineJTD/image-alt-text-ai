1b6aabc2c4ee50a3ddef0e5e1dac10a5cca97451
FelineJTD <13520050@std.stei.itb.ac.id>
2024-09-08 21:59:44 +0700

    feat: alt text prompts

--- internal-infra/graph.py
+++ internal-infra/graph.py
@@ -12,10 +12,15 @@ load_dotenv()
 
 from states import State
 
-from prompts import role_identifier_prompt
+from prompts import role_identifier_prompt, alt_text_prompts
 
 memory = SqliteSaver.from_conn_string(":memory:")
 
+# GET IMAGE CONTEXT
+def get_image_context(state: State):
+    pass
+
+# DETERMINE IMAGE WCAG ROLE
 def determine_image_role(state: State):
     role_identifier_llm = ChatOpenAI(model='gpt-4o-mini', temperature=0.5)
     role_identifier_prompt_template = PromptTemplate.from_template(role_identifier_prompt)
@@ -38,11 +43,62 @@ def determine_image_role(state: State):
 
     return {"ai_predicted_role": predicted_role}
 
+# OCR
+def ocr_image(state: State):
+    pass
+
+# NRE
+def nre_image(state: State):
+    pass
+
+# BLANK ALT TEXT
+def blank_alt_text(state: State):
+    return {"ai_predicted_alt_text": ""}
+
+# GENERATE ALT TEXT
+def generate_alt_text(state: State):
+    # Alt text generation based on guidelines from WCAG: https://www.w3.org/WAI/tutorials/images/
+
+    # If the predicted role is decorative, return an empty alt text
+    if state.ai_predicted_role == "decorative":
+        return {"ai_predicted_alt_text": ""}
+    
+    # For other roles, generate alt text based on the role
+    alt_text_generator_llm = ChatOpenAI(model='gpt-4o-mini', temperature=0.5)
+    alt_text_generator_prompt_template = PromptTemplate.from_template(alt_text_prompts[state.ai_predicted_role])
+    alt_text_generator_output_parser = StrOutputParser()
+
+    alt_text_generator_chain = alt_text_generator_prompt_template | alt_text_generator_llm | alt_text_generator_output_parser
+    predicted_alt_text = alt_text_generator_chain.invoke(
+       f"""
+        Image: {state.input_image_src}\n\n 
+        The image's file name: {state.input_image_filename}\n\n 
+        The image's attributes: {state.input_image_attrs}\n\n 
+        The image's <a> or <button> parent: {state.input_a_button_parent}\n\n 
+        The previous text before the image appears: {state.input_previous_text}\n\n 
+        The next text after the image appears: {state.input_next_text}\n\n 
+        """
+    )
+
+    pprint(f"predicted_alt_text: {predicted_alt_text}")
+    pprint(f"correct_alt_text: {state.correct_alt_text}")
+
+    return {"ai_predicted_alt_text": predicted_alt_text}
+
 # Define a new graph
 workflow = StateGraph(State)
 
 # Add the nodes to the graph
 workflow.add_node("determine_image_role", determine_image_role)
+workflow.add_node("ocr_image", ocr_image)
+workflow.add_node("nre_image", nre_image)
+workflow.add_node("blank_alt_text", blank_alt_text)
+workflow.add_node("generate_alt_text", generate_alt_text)
+
+# Add the edges to the graph
+workflow.add_edge("determine_image_role", "generate_alt_text")
+
+# Set the entry point
 workflow.set_entry_point("determine_image_role")
 
 # Finally, we compile it!
--- internal-infra/prompts.py
+++ internal-infra/prompts.py
@@ -20,8 +20,89 @@ As each role needs to be handled differently when generating alt texts, your out
 Return only the role of the image from the list above. Return the role as a single word without any enclosing bracket, e.g., informative, decorative, functional, text, or complex. THIS IS IMPORTANT! RETURN ONLY THE ROLE OF THE IMAGE.
 """
 
+
 image_description_prompt = """
 Describe the image in a few words to help identify its role. This description should be concise and capture the essence of the image. You can mention any text present in the image, the context in which the image appears, or any other relevant details that can help determine the role of the image.
 
 {message}
 """
+
+
+alt_text_prompts = {
+    "informative": """
+        The role of the image has been identified as 'informative'. Informative images convey a simple concept or information that can be expressed in a short phrase or sentence. The text alternative should convey the meaning or content that is displayed visually, which typically isn't a literal description of the image. In some situations a detailed literal description may be needed, but only when the content of the image is all or part of the conveyed information.
+
+        Example 1: Images used to label other information
+        This example includes two image icons - one of a telephone, one of a fax machine. A phone number follows each image. Consistent with the visual presentation, the text alternatives "Telephone:" and "Fax:" are used to identify the device associated with each number.
+
+        Example 2: Images used to supplement other information
+        The following example shows a dog wearing a bell. It supplements the adjacent text that explains the purpose of this bell (Off-duty guide dogs often wear a bell. Its ring helps the blind owner keep track of the dog's location). A short text alternative is sufficient to describe the information that is displayed visually but is not explained in the text; in this case, the text alternative is "Dog with a bell attached to its collar.".
+
+        Example 3: Images conveying succinct information
+        A simple diagram illustrates a counter-clockwise direction for unscrewing a bottle top or cap. The information can be described in a short sentence, so the text alternative "Push the cap down and turn it counter-clockwise (from right to left)" is given in the alt attribute.
+
+        Example 4: Images conveying an impression or emotion
+        A photograph shows a happy family group. It's a stock image so the individuals should not be identified. It's being used to give the impression that the website or the company it represents is family-friendly. The text alternative is "We're family-friendly" as this best describes the intended impression.
+
+        Example 5: Images conveying file format
+        In this example, a document is available to download in three different formats identified by format icons within text links. They have the text alternatives "HTML", "Word document", and "PDF" to distinguish the file type for each link.
+
+        Write a concise and descriptive alt text for the image based on the role of the image as 'informative' as well as the context provided from the website. The alt text should convey the meaning or content of the image in a way that is meaningful and useful to those who rely on screen readers or other assistive technologies.
+
+        Write only the alt text for the image. Do not include any additional information or context. Do not enclose the alt text in quotes. For example, if the alt text is "A dog with a bell attached to its collar.", you should only write: A dog with a bell attached to its collar.
+
+        {message}
+
+        """,
+    
+    "functional": """
+        Functional images are used to initiate actions rather than to convey information. They are used in buttons, links, and other interactive elements. The text alternative for the image should convey the action that will be initiated (the purpose of the image), rather than a description of the image.
+
+        For instance, as shown in examples below, the text alternative should be “print this page” rather than “(image of a) printer”, “search” rather than “magnifying lens” or “Example.com homepage” rather than “Example.com logo”.
+
+        Example 1: Image used alone as a linked logo
+        The following image of the W3C Logo is the only content of a link that leads to the W3C home page (no relevant next or previous text). It has the text alternative "W3C home" to indicate where the link will take the user.
+
+        Example 2: Logo image within link text
+        In this example, the W3C logo is used to supplement text within a link that leads to the W3C home page (next text is "W3C Home"). The image does not represent different functionality or convey other information than that already provided in the link text, so a null (empty) value is applied, (alt=""), to avoid redundancy and repetition. In effect the image is a decorative adjunct or visual cue to the link text.
+
+        Example 3: Icon image conveying information within link text
+        In this example, the image of a window icon follows text within a link to inform users that the link will open in a new window. It has the text alternative “new window” to convey the meaning of the icon: that activating the link will open a new browser window.
+
+        Example 4: Stand-alone icon image that has a function
+        The following image is an icon representing a printer to denote print functionality. It has the text alternative “Print this page” because its purpose is to activate the print dialog when it is selected.
+
+        Example 5: Image used in a button
+        The following image is used to give the button a distinct style. In this case, it is the button to initiate a search request and is an icon representing a magnifying lens. The text alternative for the image is “search” to convey the purpose of the button.
+
+        Write a concise and descriptive alt text for the image based on the role of the image as 'functional' as well as the context provided from the website. The alt text should convey the action that will be initiated by the image (the purpose of the image) in a way that is meaningful and useful to those who rely on screen readers or other assistive technologies.
+
+        Write only the alt text for the image. Do not include any additional information or context. Do not enclose the alt text in quotes. For example, if the alt text is "Print this page", you should only write: Print this page.
+
+        {message}
+    """,
+
+    "text": """
+        The role of the image has been identified as 'text'. Text images are those that contain readable text within the image itself. 
+
+        When an image contains text that is important to understanding the content, the alt text should contain the same words as in the image. This ensures that the information is accessible to users who cannot see the image.
+
+        Write a concise and descriptive alt text for the image based on the role of the image as 'functional' as well as the context provided from the website. The alt text should convey the action that will be initiated by the image (the purpose of the image) in a way that is meaningful and useful to those who rely on screen readers or other assistive technologies.
+
+        Write only the alt text for the image. Do not include any additional information or context. Do not enclose the alt text in quotes. For example, if the alt text is "Print this page", you should only write: Print this page.
+
+        {message}
+    """,
+
+    "complex": """
+        The role of the image has been identified as 'complex'. Complex images are those that convey detailed information or data that cannot be easily described in a few words. 
+
+        When an image contains complex information, the alt text should provide a complete text equivalent of the data or information presented in the image. This ensures that users who cannot see the image can still access the information it contains.
+
+        Write a concise and descriptive alt text for the image based on the role of the image as 'functional' as well as the context provided from the website. The alt text should convey the action that will be initiated by the image (the purpose of the image) in a way that is meaningful and useful to those who rely on screen readers or other assistive technologies.
+
+        Write only the alt text for the image. Do not include any additional information or context. Do not enclose the alt text in quotes. For example, if the alt text is "Print this page", you should only write: Print this page.
+
+        {message}
+    """,
+}
