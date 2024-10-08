2b09f8930ec24e69dd3c99902f93f784bc7c6c54
FelineJTD <13520050@std.stei.itb.ac.id>
2024-10-09 01:44:38 +0700

    feat: load more images

--- add-on/main.js
+++ add-on/main.js
@@ -3,21 +3,17 @@
 // Add-on working
 console.log("Alt text add-on working");
 
-// Get all images
-var images = document.getElementsByTagName("img");
+// Listen if there is a change in the DOM
+document.addEventListener("DOMSubtreeModified", function () {
+  // Get all images
+  var images = document.getElementsByTagName("img");
 
-// Loop through all images
-for (var i = 0; i < images.length; i++) {
-  console.log(images[i].alt);
-  // If alt text is empty, set it to "Image"
-  if (images[i].alt === "This contains an image of: ") {
-    images[i].alt = "Image";
+  // Loop through all images
+  for (var i = 0; i < images.length; i++) {
+    console.log(images[i].alt);
+    // If alt text is empty, set it to "Image"
+    if (images[i].alt === "This contains an image of: ") {
+      images[i].alt = "Image";
+    }
   }
-  // // Set the src attribute to ""
-  // images[i].src = "";
-  // // Set the srcset attribute to ""
-  // images[i].srcset = "";
-  // // Display the alt text
-  // images[i].style =
-  //   "display: block; width: 100%; height: auto; margin: 0 auto;";
-}
+});
