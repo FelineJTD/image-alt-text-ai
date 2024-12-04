98d3d2a6af4d2e5935ffa586351a0713a396cd05
FelineJTD <13520050@std.stei.itb.ac.id>
2024-12-05 00:55:31 +0700

    feat: extract context

--- extension-plasmo/content.ts
+++ extension-plasmo/content.ts
@@ -6,11 +6,11 @@ export const config: PlasmoCSConfig = {
   run_at: "document_end"
 }
 
-// Add-on working
-console.log("AI ALT TEXT: Alt text add-on working")
-
 // Backend URL
 const BACKEND_URL = "http://localhost:5000"
+
+// Add-on working
+console.log("AI ALT TEXT: Alt text add-on working")
 
 // Inject CSS to change focused element to have a purple border
 const style = document.createElement("style")
@@ -52,6 +52,99 @@ images.forEach((img) => {
   img.addEventListener("keydown", (e) => {
     if (e.key === "a" && e.ctrlKey && e.getModifierState("Alt")) {
       console.log("AI ALT TEXT: Generating alt text")
+
+      const url = window.location.href
+      console.log("AI ALT TEXT: Current URL: ", url)
+
+      // src: imageUrl,
+      // alt: imageAlt,
+      // attrs: imageAttrs,
+      // a_button_parent: aButtonParent
+      //   ? aButtonParent.outerHTML
+      //   : "None",
+      // prev_text: prevText,
+      // next_text: nextText,
+
+      // Get the image src
+      const src = img.src
+      console.log("AI ALT TEXT: Image src: ", src)
+
+      // Get the image alt text
+      const alt = img.alt
+      console.log("AI ALT TEXT: Image alt: ", alt)
+
+      // Get the image attributes
+      const attrs = img.attributes
+      console.log("AI ALT TEXT: Image attributes: ", attrs)
+
+      // Search 3 levels of parent elements for an <a> or <button> tag
+      let aButtonParent = null
+      let parent = img.parentElement
+      for (let i = 0; i < 3; i++) {
+        if (parent.tagName === "A" || parent.tagName === "BUTTON") {
+          aButtonParent = parent
+          break
+        }
+        parent = parent.parentElement
+      }
+      console.log(
+        "AI ALT TEXT: Parent with <a> or <button> tag: ",
+        aButtonParent
+      )
+
+      // Search 5 levels of previous sibling elements for text
+      let i = 0
+      let prevText = ""
+      let currElement = img as HTMLElement
+      while (i < 5) {
+        // If there is a previous sibling, check if it contains text
+        if (currElement.previousSibling) {
+          console.log(
+            "AI ALT TEXT: Previous sibling: ",
+            currElement.previousSibling
+          )
+          prevText = currElement.previousSibling.textContent
+
+          // Clean the previous text (trim, remove new lines, make multiple spaces into one)
+          prevText = prevText.trim().replace(/\n/g, " ").replace(/\s+/g, " ")
+          // Break if text is found
+          if (prevText.length > 0) {
+            break
+          }
+          currElement = currElement.previousSibling as HTMLElement
+          i++
+        } else {
+          // Search parent element for text
+          currElement = currElement.parentElement as HTMLElement
+        }
+      }
+      console.log("AI ALT TEXT: Previous text: ", prevText)
+
+      // Search 5 levels of next sibling elements for text
+      i = 0
+      let nextText = ""
+      currElement = img as HTMLElement
+      while (i < 5) {
+        // If there is a next sibling, check if it contains text
+        if (currElement.nextSibling) {
+          console.log("AI ALT TEXT: Next sibling: ", currElement.nextSibling)
+          nextText = currElement.nextSibling.textContent
+
+          // Clean the next text (trim, remove new lines, make multiple spaces into one)
+          nextText = nextText.trim().replace(/\n/g, " ").replace(/\s+/g, " ")
+          // Break if text is found
+          if (nextText.length > 0) {
+            break
+          }
+          currElement = currElement.nextSibling as HTMLElement
+          i++
+        } else {
+          // Search parent element for text
+          currElement = currElement.parentElement as HTMLElement
+        }
+      }
+      console.log("AI ALT TEXT: Next text: ", nextText)
+
       // Send image URL to backend
       // fetch(`${BACKEND_URL}/image`, {
       //   method: "POST",
@@ -90,54 +183,35 @@ images.forEach((img) => {
   })
 })
 
-// Make all images focusable and with purple border when focused whenever there is an update to the DOM
-const observer = new MutationObserver((mutations) => {
-  console.log("AI ALT TEXT: DOM updated")
-  mutations.forEach((mutation) => {
-    mutation.addedNodes.forEach((node) => {
-      if (node instanceof HTMLImageElement) {
-        console.log("AI ALT TEXT: Image added")
-        node.tabIndex = 0
-        node.addEventListener("focus", () => {
-          node.classList.add("img-focused")
-        })
-        node.addEventListener("blur", () => {
-          node.classList.remove("img-focused")
-        })
-      }
-    })
-  })
-})
-
 // Detect if user focuses on an image
-document.addEventListener(
-  "focus",
-  (e) => {
-    if (e.target instanceof HTMLImageElement) {
-      console.log("AI ALT TEXT: Image focused")
-      // Get the image src
-      const src = e.target.src
-      console.log("AI ALT TEXT: Image src: ", src)
-      // Send image URL to backend
-      //   fetch(`${BACKEND_URL}/image`, {
-      //     method: "POST",
-      //     headers: {
-      //       "Content-Type": "application/json"
-      //     },
-      //     body: JSON.stringify({
-      //       url: e.target.src
-      //     })
-      //   })
-      //     .then((response) => response.json())
-      //     .then((data) => {
-      //       console.log(data)
-      //       // Set alt text to image
-      //       e.target.alt = data.alt
-      //     })
-      // }
-    }
-  },
-  true
-)
+// document.addEventListener(
+//   "focus",
+//   (e) => {
+//     if (e.target instanceof HTMLImageElement) {
+//       console.log("AI ALT TEXT: Image focused")
+//       // Get the image src
+//       const src = e.target.src
+//       console.log("AI ALT TEXT: Image src: ", src)
+//       // Send image URL to backend
+//       //   fetch(`${BACKEND_URL}/image`, {
+//       //     method: "POST",
+//       //     headers: {
+//       //       "Content-Type": "application/json"
+//       //     },
+//       //     body: JSON.stringify({
+//       //       url: e.target.src
+//       //     })
+//       //   })
+//       //     .then((response) => response.json())
+//       //     .then((data) => {
+//       //       console.log(data)
+//       //       // Set alt text to image
+//       //       e.target.alt = data.alt
+//       //     })
+//       // }
+//     }
+//   },
+//   true
+// )
 
 export {}
